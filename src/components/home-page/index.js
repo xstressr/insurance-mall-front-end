88e8a2e8d120ddc59c8007addfe0094b4cf8e953
Brian Oster <xstress@outlook.com>
2021-11-03 22:35:00 +0800

    修改顶部条

--- src/components/home-page/index.js
+++ src/components/home-page/index.js
@@ -1,14 +1,15 @@
-import { Carousel } from 'antd';
 import React from 'react'
-import { HomeWrapper } from './style'
-import { VerifiedOutlined, ShopOutlined, SecurityScanOutlined, RedEnvelopeOutlined} from '@ant-design/icons'
 
-function onChange(a, b, c) {
-  console.log(a, b, c);
-}
+import { Carousel, Tabs, Button, Divider } from 'antd';
+import { VerifiedOutlined, ShopOutlined, SecurityScanOutlined, RedEnvelopeOutlined } from '@ant-design/icons'
+
+
+import { HomeWrapper } from './style'
+import astronaut from "@/assets/img/astronaut.jpg"
+import pexels from "@/assets/img/pexels.jpg"
 
 const contentStyle = {
-  height: '200px',
+  height: '300px',
   color: '#fff',
   // lineHeight: '160px',
   textAlign: 'center',
@@ -18,50 +19,159 @@ const contentStyle = {
 
 
 export default function Home() {
+  const { TabPane } = Tabs;
   return (
     <HomeWrapper>
       <div className="header">
         <div className="header-top">
-          <div className="header-left">欢迎登陆JOJO保险商城</div>
+          <div className="header-left">
+          <ul className="left-item">
+            <li><a href="/" className="login">欢迎登陆JOJO保险商城</a></li>
+            <li></li>
+            <li className="tooltipBox">
+              <a>我的</a>
+              <div className="myTooltip">
+                <a>我的订单</a>
+                <a>我的购物车</a>
+                <a>快速理赔</a>
+                <a>服务记录</a>
+                <a>核保记录</a>
+              </div>
+            </li>
+            <li></li>
+            <li>商家入住</li>
+            <li></li>
+            <li>积分商城</li>
+            <li></li>
+            <li>保单查询</li>
+            <li></li>
+          </ul>
+          
+          </div>
           <div className="header-right">
-            <a href="/">登陆/注册</a>
+            <a href="/" className="login">登陆/注册</a>
           </div>
         </div>
       </div>
       <div className="header-bottom">
-        <div style={{display:"flex"}}>
+        <div style={{ display: "flex" }}>
           <div>
-            <VerifiedOutlined style={{fontSize:"30px",margin:"0 10px"}}/>
+            <VerifiedOutlined style={{ fontSize: "30px", margin: "0 10px" }} />
           </div>
           <div>
-          <ShopOutlined style={{fontSize:"30px",margin:"0 10px"}}/>
+            <ShopOutlined style={{ fontSize: "30px", margin: "0 10px" }} />
           </div>
           <div>
-          <SecurityScanOutlined style={{fontSize:"30px",margin:"0 10px"}}/>
+            <SecurityScanOutlined style={{ fontSize: "30px", margin: "0 10px" }} />
 
           </div>
           <div>
-        <RedEnvelopeOutlined style={{fontSize:"30px",margin:"0 10px"}}/>
+            <RedEnvelopeOutlined style={{ fontSize: "30px", margin: "0 10px" }} />
 
           </div>
         </div>
 
       </div>
       <div className="carousel">
-        <Carousel afterChange={onChange}>
+        <Carousel autoplay>
           <div>
-            <h3 style={contentStyle}>1</h3>
+            <h3 style={contentStyle}>
+              <img src={astronaut} alt="轮播图1"></img>
+            </h3>
           </div>
           <div>
-            <h3 style={contentStyle}>2</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>3</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>4</h3>
+            <h3 style={contentStyle}>
+              <img src={pexels} alt="轮播图2"></img>
+            </h3>
           </div>
         </Carousel>
+      </div>
+
+      <div className="content">
+        <div className="content-top">
+          <div className="content-top-left">
+            <div className="content-top-left-title" >
+              <Tabs defaultActiveKey="1" >
+                <TabPane tab="人身保障" key="1">
+                  <a>成人保险</a>
+                  <div style={{display:"inline-block"}}>
+                    <a>test </a>
+                    <a>test </a>
+                    <a>test </a>
+                  </div>
+                </TabPane>
+                <TabPane tab="财富保险" key="2">
+                  
+                <a>test保险</a>
+                  <div style={{display:"inline-block"}}>
+                    <a>test </a>
+                    <a>test </a>
+                    <a>test </a>
+                  </div>
+                </TabPane>
+                <TabPane tab="旅游出行" key="3">
+                  
+                <a>小孩保险</a>
+                  <div style={{display:"inline-block"}}>
+                    <a>test </a>
+                    <a>test </a>
+                    <a>test </a>
+                  </div>
+                </TabPane>
+                <TabPane tab="全部产品" key="4">
+                <a>全部保险</a>
+                  <div style={{display:"inline-block"}}>
+                    <a>test </a>
+                    <a>test </a>
+                    <a>test </a>
+                  </div>
+                </TabPane>
+              </Tabs>
+            </div>
+          </div>
+          <div className="content-top-right">
+            <div className="content-top-right-title">
+              <div className="pic">
+                <img src="//img.huizecdn.com/hz/www/page/core_pc/avator.png" alt="头像" style={{ width: "48px", height: "48px" }} />
+              </div>
+              <div className="content-top-right-header">Hi~登录发现更多精彩</div>
+              <div className="content-top-right-header">
+                <a href="/">
+                  <Button type="primary" >
+                    <span>登录 / 注册</span>
+                  </Button>
+                </a>
+                <Button >
+                  <span>新人福利</span>
+                </Button>
+              </div>
+
+            </div>
+            {/* <Divider /> */}
+            <div>
+              <div >
+                <h4  href="https://www.huize.com/hz-planet/lanmu/tuijian/0">
+                  <span style={{fontSize:"16px"}}>保险头条</span>
+                  <a  href="https://www.huize.com/hz-planet/lanmu/tuijian/0" target="_blank">
+                    <span>更多</span>
+                    <div >
+                    </div>
+                  </a>
+                </h4>
+                <a  target="_blank"  href="//www.huize.com/hz-planet/article/15397">
+                  <img src="//img.huizecdn.com/hz/www/page/core_pc/1.png" alt="" style={{width:"7px",marginLeft:"3px"}} />
+                  <span>又要年度体检了，这件事情赶紧做！</span>
+                </a>
+                <a  target="_blank"  href="//www.huize.com/hz-planet/article/15396">
+                  <img src="//img.huizecdn.com/hz/www/page/core_pc/2.png" alt="" style={{width:"7px",marginLeft:"3px"}} />
+                  <span>穗岁康2022强势回归！245万保额，有既往病史也能赔！</span>
+                </a><a  href="//www.huize.com/hz-planet/article/15398">
+                  <img src="//img.huizecdn.com/hz/www/page/core_pc/3.png" alt="" style={{width:"7px",marginLeft:"3px"}} />
+                  <span>北京人寿超好保，要超越达尔文5号换新版了！</span></a></div>
+
+            </div>
+          </div>
+        </div>
       </div>
     </HomeWrapper>
   )
--- src/components/home-page/style.js
+++ src/components/home-page/style.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
+  
+  background-color: rgb(246, 248, 252);
+  
 
   .header {
     height: 30px;
-    background-color: rgb(245,245,245);
+    background-color: rgb(43,50,62);
     
     .header-top {
     width: 1200px;
@@ -15,12 +18,60 @@ export const HomeWrapper = styled.div`
 
     .header-left {
       font-size: 12px;
+      color: rgb(100, 110, 120);
       line-height: 30px;
+      .left-item {
+        display: flex;
+
+        li:nth-of-type(2n) {
+          height: 8px;
+          background-color: #5d687a;
+          width: 1px;
+          margin: 16px 12px 0;
+        }
+        
+        .tooltipBox:hover .myTooltip  
+        {
+            visibility: visible;
+        }
+
+        .myTooltip {
+          visibility: hidden;
+          position: absolute;
+          z-index: 100;
+          background: #fff;
+          border: 1px solid #e6ecf6;
+          box-shadow: 0 0 8px 0 rgb(0 0 0 / 10%);
+          a {
+            display: block;
+            width: 80px;
+            height: 32px;
+            line-height: 32px;
+            text-align: center;
+
+          }
+        }
+
+        
+      }
+      
+      a {
+        color: rgb(100, 110, 120);
+      }
+
+      a:hover {
+        color: #fff
+      }
     }
 
     .header-right {
       font-size: 12px;
+      color: rgb(95, 102, 115);
       line-height: 30px;
+      
+      a:hover {
+        color: #fff
+      }
     }
   }
 
@@ -42,5 +93,47 @@ export const HomeWrapper = styled.div`
     width: 1200px;
     height: 300px;
     margin: auto;
+  }
+
+  .content {
+    width: 1200px;
+    margin: auto;
+
+    .content-top {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 10px;
+
+      .content-top-left {
+        width: 890px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 15px;
+
+        .content-top-left-title {
+          padding-left: 10px;
+        }
+      }
+
+      .content-top-right {
+        width: 300px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 15px;
+
+        .content-top-right-title {
+          .pic {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          
+          .content-top-right-header{
+            text-align: center;
+          }
+        }
+      }
+
+    }
   }
 `
